079a35517d732a626e750e8ce04cbf335aaa8b81
Janis Cheng <janischeng@Janiss-MacBook-Air.local>
2021-02-10 15:53:26 -0500

    Making the sequences dataframe

--- Download.R
+++ Download.R
@@ -1,6 +1,34 @@
-install.packages("rentrez")
-library(rentrez)
+#Assignment 4
+ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1") #define ncbi id vectors
+ibrary(rentrez) #loading the library
+Bburg<-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") #download data of the specific ncbi identifiers on the NCBI database
+#this contains 3 different sequences of the 16S gene of Borrelia burgdorferi (bacteria that causes Lyme Disease)
 
-ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1")
+Bburg #checking it
+#they are one after the other and not separated 
 
-install.packages("devtools")
+Sequences <- strsplit(Bburg, "\n\n") #separating each sequence into an indipendent element
+print(Sequences)
+
+#the [[1]] at the top means this is a list (strsplit outputs into a list)
+#convert this list into not a list
+Sequences<-unlist(Sequences)
+Sequences #take a look at it 
+
+#use regular expressions to separate the sequences from the headers
+header<-gsub("(^>.*sequence)\\n[ATCG].*","\\1",Sequences) #look for each sequence and capture the beginning before the sequence starts, make it header
+#[ATCG] refers to each sequence blurb
+seq<-gsub("^>.*sequence\\n([ATCG].*)","\\1",Sequences) #find and capture the actual sequence
+Sequences<-data.frame(Name=header,Sequence=seq) #make a dataframe
+Sequences #take a look, it has \n new line characters (the sequences are cut into different lines)
+
+#to remove the newline characters:
+library(dplyr) #load dplyr
+Sequences<- transmute (Sequences, Name, Sequence= gsub ("\\n", "",Sequences$Sequence)) #remaking the columns using dplyr and using regex in it
+#so we can apply gsub only to the sequences column and not the entire dataframe
+Sequences #check if the newlines disappeared
+
+write.csv(Sequences, "./Sequences.csv",row.names = FALSE)
+#Now remove the newline characters from the Sequences data frame using regular expressions
+
+
